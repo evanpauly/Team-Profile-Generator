63767fbfbe74570968d17cef71f69af871071b6e
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-03-03 11:38:12 -0600

    getting started

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 const fs = require('fs');
+const inquirer = require('inquirer');
+
+const propmtUser = () => {
+    return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your name!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'What is your ID? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your ID!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your email address!');
+            return false;
+          }
+        }
+      }
+    ]);
+};
